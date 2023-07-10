a3c166efa95d9f3334bc779912f8bd7db0d20d39
Wexito11 <wexler.a.g.p@gmail.com>
2023-07-10 11:26:15 +0300

    Cambio divisa

--- Bot_1.py
+++ Bot_1.py
@@ -3,25 +3,78 @@ from telebot import types
 import webbrowser
 import sqlite3
 import requests
-import json
+#import json
+from currency_converter import CurrencyConverter
 
 bot = telebot.TeleBot('6134115976:AAHpQeeXxx-D7u7gniTb9ojK7hbFYlK5MgM')
 API = 'c3c6c5c894f602c593b8591f6e2ae976'
+currency = CurrencyConverter()
+name = None
+amount = 0
 
 def Nonone(Text):
     if Text==None:
         return ""
     else:
         return Text
-    
+
 #Comandos que inician con "/""
 #Abrir sitio web
 @bot.message_handler(commands = ['site', 'website'])
 def site(message):
     webbrowser.open('http://vk.com')
 
-name = None
+# Conversión de divisas
+@bot.message_handler(commands = ['start3'])
+def start3(message):
+    bot.send_message(message.chat.id, 'Привет, введите сумму')
+    bot.register_next_step_handler(message, summa)
 
+def summa(message):
+    global amount
+    #Solo aceptar numeros positivos
+    try:
+        amount = int(message.text.strip())
+    except ValueError:
+        bot.send_message(message.chat.id, 'Неверный формат, впишите сумму')
+        bot.register_next_step_handler(message, summa)
+        return
+
+    if amount>0:
+        markup = types.InlineKeyboardMarkup(row_width=2)
+        btn1 = types.InlineKeyboardButton('USD/EUR', callback_data='usd/eur')
+        btn2 = types.InlineKeyboardButton('EUR/USD', callback_data='eur/usd')
+        btn3 = types.InlineKeyboardButton('USD/GBP', callback_data='usd/gbp')
+        btn4 = types.InlineKeyboardButton('Другое значение', callback_data='else')
+        markup.add(btn1, btn2, btn3, btn4)
+        bot.send_message(message.chat.id, 'Выберите пару балют', reply_markup=markup)
+    else: 
+        bot.send_message(message.chat.id, 'Число должно быть больше за 0, впишите сумму')
+        bot.register_next_step_handler(message, summa)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback1(call):
+    if call.data != 'else':
+        values = call.data.upper().split('/')
+        res = currency.convert(amount, values[0], values[1])
+        bot.send_message(call.message.chat.id, f'Получается: {round(res, 2)}. Можете засново вписать сумму')
+        bot.register_next_step_handler(call.message, summa)
+    else:
+        bot.send_message(call.message.chat.id, 'Выберите пару значение через слэш')
+        bot.register_next_step_handler(call.message, my_currency)
+
+def my_currency(message):
+    try:
+        values = message.text.upper().split('/')
+        res = currency.convert(amount, values[0], values[1])
+        bot.send_message(message.chat.id, f'Получается: {round(res, 2)}. Можете засново вписать сумму')
+        bot.register_next_step_handler(message, summa)
+    #Cualquier excepcion
+    except Exception:
+        bot.send_message(message.chat.id, 'Что-то не так. Впишите значение заново')
+        bot.register_next_step_handler(message, my_currency)
+    
+"""
 #Clima https://home.openweathermap.org/api_keys
 @bot.message_handler(commands = ['start2'])
 def start2(message):
@@ -32,15 +85,15 @@ def get_weather(message):
     city = message.text.strip().lower()
     res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric')
     if res.status_code == 200:
-       data = json.loads(res.text)
-       temp = data["main"]["temp"]
-       bot.reply_to(message, f'Сейчас погода: {temp}')
-       image = 'Soleado.png' if temp > 5.0 else 'Nublado con sol.png'
-       file = open('./' + image, 'rb')
-       bot.send_photo(message.chat.id, file)
+        data = json.loads(res.text)
+        temp = data["main"]["temp"]
+        bot.reply_to(message, f'Сейчас погода: {temp}')
+        image = 'Soleado.png' if temp > 5.0 else 'Nublado con sol.png'
+        file = open('./' + image, 'rb')
+        bot.send_photo(message.chat.id, file)
     else:
         bot.reply_to(message, 'Город указан не верно') 
-
+"""
 
 #Подключение к базе данных
 @bot.message_handler(commands = ['start1'])
